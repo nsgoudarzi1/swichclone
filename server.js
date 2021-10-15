919a46180702fe0bec7f7d92b0757f87954ded71
nsgoudarzi1 <69822293+nsgoudarzi1@users.noreply.github.com>
2021-10-15 15:56:42 -0500

    1

--- server.js
+++ server.js
@@ -35,14 +35,13 @@ const users = require('./routes/user');
 app.use('/users', users);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  app.use("/swichclone/", express.static("client/build"));
+if (process.env.NODE_ENV = "production") {
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
 // Starts the server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
