48c289db5795a8105137c33376ac5d04ea80cc2d
nsgoudarzi1 <69822293+nsgoudarzi1@users.noreply.github.com>
2021-10-16 00:23:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ app.use('/users', users);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV = "production") {
-  app.use(express.static('client/build'));
-
+  app.use(express.static(path.join('client/build')));
+  
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
-}
+  }
 
 // Starts the server
 app.listen(port, () => {
